7b53691e8c948ac4e641b58311275ebc77edf81c
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2024-09-22 11:18:39 -0600

    [UPDATE]
    se agregaron CORS

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from app.auth import auth_bp
 from app.admin import admin_bp
 from config import load_config
 from app.email import init_mail  # Importa la función para inicializar Flask-Mail
 app = Flask(__name__)
 
+CORS(app, resources={r"/*": {"origins": "*"}})
 load_config(app)
-# Inicializa Flask-Mail
 init_mail(app)
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(admin_bp, url_prefix='/admin')
