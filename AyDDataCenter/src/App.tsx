129075744fb4db7eba6a4f66edc27fe112514339
Pedro <2574151141308@ingenieria.usac.edu.gt>
2024-09-23 21:20:57 -0600

    [UPDATE] actualizaion de APP

--- AyDDataCenter/src/App.tsx
+++ AyDDataCenter/src/App.tsx
@@ -1,12 +1,14 @@
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes/AppRouter'
+import { AuthProvider } from './context/AuthContext'
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <>
-    <div className=" bg-slate-800 h-screen">
-      <h1 className="text-blue-50 font-sans font-extralight text-5xl" >AyD Data Center</h1>
-    </div>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
