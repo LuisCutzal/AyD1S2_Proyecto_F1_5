973ebdb2338e8b22aca7b43ccb56c6321abef1cf
Pedro <2574151141308@ingenieria.usac.edu.gt>
2024-09-23 21:34:19 -0600

    [UPDATE] integracion de endpoints usuario

--- AyDDataCenter/src/services/authService.ts
+++ AyDDataCenter/src/services/authService.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
-
+import { ForgotPasswordResponse } from '../types/Auth'
 /**
  * Registrar un nuevo usuario (cliente).
  * @param userData - Datos del usuario a registrar.
@@ -29,12 +29,17 @@ export const registerUser = async (userData: UserData) => {
       })
   
       if (!response.ok) {
+        if (response.status === 201) {
+          const data = await response.json()
+          return data
+        }
+        if (response.status === 409) {
+          throw new Error('El nombre de usuario o correo ya está en uso.')
+        }
         const errorData = await response.json()
         throw new Error(errorData.Error || 'Error al registrar usuario.')
-      }
-  
-      const data = await response.json()
-      return data
+      } 
+      return 
     } catch (error) {
       console.error('Error en registro:', error)
       throw error
@@ -50,26 +55,29 @@ export const registerUser = async (userData: UserData) => {
    */
   export const loginUser = async (credentials: { identificador: string, contrasena: string }) => {
     try {
-      const response = await fetch(`${API_BASE_URL}/auth/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(credentials),
-      })
+      // const response = await fetch(`${API_BASE_URL}/auth/login`, {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify(credentials),
+      // })
   
-      const data = await response.json()
+      // const data = await response.json()
   
-      if (!response.ok) {
-        throw new Error(data.error || 'Error en el inicio de sesión.')
-      }
-  
-      const { token } = data
-  
+      // if (!response.ok) {
+      //   throw new Error(data.error || 'Error en el inicio de sesión.')
+      // }
+      console.log(credentials)
+      const data = {
+        message: "Login successful",
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF91c3VhcmlvIjoxMSwiaWRfcm9sIjoxLCJub21icmVfdXN1YXJpbyI6Im5vb29vbyJ9.0inzZBQLd5dFF41vdZwsituroIBv7ITOR2a_XAJTBe8"
+        }
+
+      const { token, message } = data
       // Almacenar el token en localStorage para futuras peticiones
       localStorage.setItem('authToken', token)
-  
-      return data
+      return {message}
     } catch (error) {
       console.error('Error en login:', error)
       throw error
@@ -82,28 +90,98 @@ export const registerUser = async (userData: UserData) => {
    * @throws Error en caso de falla en la petición.
    */
 
+  interface UserData {
+    apellido: string;
+    celular: string;
+    email: string;
+    id_rol: number;
+    id_usuario: number;
+    nacionalidad: string;
+    nombre: string;
+    nombre_usuario: string;
+    pais_residencia: string;
+    role?: string;
+  }
+
   export const getUserData = async () => {
     try {
-      const token = localStorage.getItem('authToken')
+      // const token = localStorage.getItem('authToken')
   
-      if (!token) {
-        throw new Error('No hay token de autenticación.')
+      // if (!token) {
+      //   throw new Error('No hay token de autenticación.')
+      // }
+  
+      // const response = await fetch(`${API_BASE_URL}/auth/me`, {
+      //   headers: {
+      //     Authorization: `Bearer ${token}`,
+      //   },
+      // })
+  
+      // if (!response.ok) {
+      //   throw new Error('Error al obtener datos del usuario.')
+      // }
+  
+      // const data = await response.json()
+      
+
+      const data: UserData = 
+      {
+          "apellido": "Lopez",
+          "celular": "98765432",
+          "email": "maria.lopez@example.com",
+          "id_rol": 3,
+          "id_usuario": 12,
+          "nacionalidad": "Mexico",
+          "nombre": "Maria",
+          "nombre_usuario": "mlopez", //username
+          "pais_residencia": "Mexico",
+          "contrasena": "dummyPassword",
+          "espacio_asignado": 100
       }
-  
-      const response = await fetch(`${API_BASE_URL}/auth/me`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-  
-      if (!response.ok) {
-        throw new Error('Error al obtener datos del usuario.')
+      if (data.id_rol === 1) {
+        data.role = 'Administrador'
+      } else if (data.id_rol === 2) {
+        data.role = 'Empleado'
+      } else {
+        data.role = 'Usuario'
       }
-  
-      const data = await response.json()
       return data
     } catch (error) {
       console.error('Error en getUserData:', error)
       throw error
     }
   }
+
+  /**
+   * Cerrar sesión del usuario.
+   * @returns Mensaje de éxito.
+   * @throws Error en caso de falla en la petición.
+   **/ 
+
+  interface ForgotPasswordData {
+    email: string;
+  }
+
+  //Current this endpoint is not implemented in the backend
+  export const forgotPassword = async (data: ForgotPasswordData): Promise<ForgotPasswordResponse> => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/auth/forgot_password`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+  
+      if (response.ok) {
+        const resData: ForgotPasswordResponse = await response.json();
+        return resData;
+      } else {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Error al procesar la solicitud.');
+      }
+    } catch (error) {
+      console.error('Error en forgotPassword:', error);
+      throw error;
+    }
+  };
