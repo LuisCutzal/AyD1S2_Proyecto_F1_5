63175f17d4c266f07e33612fbf3d700ff6d4a933
Pedro <2574151141308@ingenieria.usac.edu.gt>
2024-09-23 21:20:36 -0600

    [UPDATE] main

--- AyDDataCenter/src/main.tsx
+++ AyDDataCenter/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+//local imports
 import App from './App.tsx'
 import './index.css'
+import ErrorBoundary from './components/common/ErrorBoundary.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+  </StrictMode>
 )
