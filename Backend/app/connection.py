7cfe55ef34856922c699ce54ea3784b3a2b64f2c
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2024-09-26 08:40:39 -0600

    [UPDATE]
    se cambio la conexion a la base de datos para una rds en aws

--- Backend/app/connection.py
+++ Backend/app/connection.py
@@ -1,12 +1,21 @@
 import pyodbc
 from dotenv import load_dotenv  # Importamos python-dotenv para cargar el .env
 import os
-# Configuración de la conexión a la base de datos
+
+# Cargar variables de entorno
 load_dotenv()
+
 def get_db_connection():
     try:
-        # Conexión al servidor SQL con autenticación de Windows
-        conexion = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={os.getenv('SERVER')};DATABASE={os.getenv('DATABASE')};Trusted_Connection=yes;', autocommit=True)
+        # Conexión al servidor SQL con autenticación SQL
+        connection_string = (
+            f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+            f'SERVER={os.getenv("SERVER")};'
+            f'DATABASE={os.getenv("DATABASE")};'
+            f'UID={os.getenv("USERNAME")};'
+            f'PWD={os.getenv("PASSWORD")};'
+        )
+        conexion = pyodbc.connect(connection_string, autocommit=True)
         return conexion
     except pyodbc.Error as e:
         print(f"Error al conectar a SQL Server: {e}")
