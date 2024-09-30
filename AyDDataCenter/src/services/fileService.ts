e3f0c540f2142a4f52b820c19e7bf586c1c46bc4
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2024-09-30 11:42:04 -0600

    [UPDATE]
    se actualizaron varios enlaces hacia la API en python

--- AyDDataCenter/src/services/fileService.ts
+++ AyDDataCenter/src/services/fileService.ts
@@ -27,25 +27,25 @@ interface DashboardData {
    */
 export const getDashboardData = async (): Promise<DashboardData> => {
     try {
-        // const token = localStorage.getItem('authToken')
+        const token = localStorage.getItem('authToken')
 
-        // if (!token) {
-        //     throw new Error('No hay token de autenticación.')
-        // }
+        if (!token) {
+            throw new Error('No hay token de autenticación.')
+        }
 
-        // const response = await fetch(`${API_BASE_URL}/cliente/dashboard`, {
-        //     method: 'GET',
-        //     headers: {
-        //         Authorization: `Bearer ${token}`,
-        //     },
-        // })
+        const response = await fetch(`${API_BASE_URL}/cliente/dashboard`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
 
-        // if (!response.ok) {
-        //     throw new Error('Error al obtener datos del dashboard.')
-        // }
+        if (!response.ok) {
+            throw new Error('Error al obtener datos del dashboard.')
+        }
 
-        // const data: DashboardData = await response.json()
-        const data = dataDashboardUser
+        const data: DashboardData = await response.json()
+        //const data = dataDashboardUser
         return data
     } catch (error) {
         console.error('Error en getDashboardData:', error)
