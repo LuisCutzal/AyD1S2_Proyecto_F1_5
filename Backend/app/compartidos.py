53842aa5f14e94c0aefe945808f5666d6125c60e
Victor Gomez <2306919780101@ingenieria.usac.edu.gt>
2024-10-11 16:42:09 -0600

    [ADD] ENDPOINT no_compartir_con
    Se agrega el ENDPOINT no compartir con para la función de compartidos.

--- Backend/app/compartidos.py
+++ Backend/app/compartidos.py
@@ -9,41 +9,81 @@ from app.decorators import token_required
 
 
 compartidos_bp = Blueprint('compartidos', __name__)
-#crear capeta
-@compartidos_bp.route('/compartir', methods=['POST'])
-#@token_required
+@compartidos_bp.route('/compartir_con', methods=['POST'])
+@token_required
 
-def compartir():
-
+def compartir_con():
     try:
         datos = request.json
-        
         id_carpeta = datos.get('id_carpeta')
         id_archivo = datos.get('id_archivo')
         id_usuario_propietario = datos.get('id_usuario_propietario')
         compartir_con = datos.get('compartir_con')
-
         conn = get_db_connection()
         cursor = conn.cursor()
-
         for id_usuario_destinatario in compartir_con:
             sql = '''
             INSERT INTO Compartidos (id_usuario_propietario, id_usuario_destinatario, id_carpeta, id_archivo, fecha_comparticion,estado)
-            VALUES (?, ?, ?, ?, ?,?)
+            VALUES (?, , ?, ?, ?,?)
             '''
-            # Insertar id_carpeta_archivo en la columna correspondiente a id_carpeta
             cursor.execute(sql, (id_usuario_propietario, id_usuario_destinatario, id_carpeta,id_archivo, datetime.now(),'A'))
-
-
-    # Confirmar las inserciones en la base de datos
         conn.commit()
-
         cursor.close()
         conn.close()
-
         return {'mensaje': 'Archivos compartidos exitosamente'}, 201
     except Exception as e:
-            # Manejar cualquier excepción que ocurra y hacer rollback en caso de error
-            #conn.rollback()
-            print("e" , e)
             return jsonify({"error": "Hubo un problema al compartir los archivos. Inténtalo nuevamente."}), 500
+    
+compartidos_bp = Blueprint('compartidos', __name__)
+@compartidos_bp.route('/no_compartir_con', methods=['POST'])
+#@token_required
+
+def no_compartir_con():
+    try:
+        datos = request.json
+        id_carpeta = datos.get('id_carpeta')
+        id_archivo = datos.get('id_archivo')
+        id_usuario_propietario = datos.get('id_usuario_propietario')
+        no_compartir_con = datos.get('no_compartir_con')
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        if id_carpeta is not None:
+            
+            for id_usuario_destinatario in no_compartir_con:
+                sql = '''
+                UPDATE Compartidos
+                SET estado = 'I'
+                WHERE id_carpeta = ? AND id_usuario_propietario = ? AND id_usuario_destinatario = ?
+                '''
+                cursor.execute(sql, (id_carpeta, id_usuario_propietario, id_usuario_destinatario))
+        
+        if id_archivo is not None:
+            
+            for id_usuario_destinatario in no_compartir_con:
+                sql = '''
+                UPDATE Compartidos
+                SET estado = I
+                WHERE id_archivo = ? AND id_usuario_propietario = ? AND id_usuario_destinatario = ?
+                '''
+                cursor.execute(sql, (id_archivo, id_usuario_propietario, id_usuario_destinatario))
+        
+        
+        if id_carpeta and id_archivo is not None:
+
+            for id_usuario_destinatario in no_compartir_con:
+                sql = '''
+                UPDATE Compartidos
+                SET estado = I
+                WHERE id_carpeta = ? AND id_archivo = ? AND id_usuario_propietario = ? AND id_usuario_destinatario = ?
+                '''
+                cursor.execute(sql, (id_carpeta,id_archivo, id_usuario_propietario, id_usuario_destinatario))
+        
+        
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return {'mensaje': 'Los archivos se dejaron de compartir exitosamente'}, 201
+    except Exception as e:
+            print("e",e)
+            return jsonify({"error": "Hubo un problema al dejar de compartir los archivos. Inténtalo nuevamente."}), 500
